0e867a5cb48afe41690357c41613cef4b9a2b86c
carolina <carolina.vrendo@gmail.com>
2021-04-02 15:15:14 -0400

    commit inicial do inicio do site

---
+++ public/front/src/app/home/home.page.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  data: string;
+
+  constructor(private http: HttpClient) {
+
+    this.data = '';
+  }
+
+  ionViewWillEnter() {
+    // Load the data
+    this.prepareDataRequest()
+      .subscribe(
+        data => {
+          // Set the data to display in the template
+          this.data = JSON.stringify(data);
+        }
+      );
+  }
+
+  private prepareDataRequest(): Observable<object> {
+    // Define the data URL
+    const dataUrl = 'https://api.nigelbpeck.com/';
+
+    const dataUrl2 = 'http://localhost:8888/api/V1/categories/list';
+ 
+    // Prepare the request
+    return this.http.get(dataUrl2);
+  }
+
+}
